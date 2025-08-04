52ee5d58a16a655c79765bb5e42baedeba8c6a23
MrTinEU <senkyr.martin@gmail.com>
2025-08-04 18:20:49 +0200

    bububu

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to My Website</h1>
+    <p>This is the homepage.</p>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    defaultLocale: 'sk',
+    locales: [
+      { code: 'sk', name: 'Slovenčina', file: 'sk.json', language: 'sk-SK' },
+      { code: 'en', name: 'English', file: 'en.json', language: 'en-US' },
+    ]
+  }
+});
