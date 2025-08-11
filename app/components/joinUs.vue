b7a6c376944bd83449a81341390ef46201ff0769
PetrBalabolkin <peterbalabolkin@gmail.com>
2025-08-11 19:08:22 +0200

    feat: add support page

---
+++ app/components/joinUs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex mx-auto justify-center items-center md:mt-[100px] mt-[60px] overflow-hidden">
+    <div class="bg-mcs-blue sm:pt-[90px] sm:pb-[100px] pb-[60px] pt-[50px] md:h-[370px] h-[430px] xl:px-[50px] lg:px-[40px] px-5 rounded xl:w-[1280px] lg:w-[980px] w-full cont">
+      <div class="flex flex-col md:gap-6 gap-5 lg:w-[680px] sm:w-[500px] w-full overflow-hidden">
+        <div class="flex flex-col gap-4">
+          <h2 class="md:text-[40px] text-3xl text-txt-white-prim">Pridaj sa k nám</h2>
+          <p class="text-txt-white-sec sm:text-[18px]">Chceš zažiť konferencie, získať skúsenosti a byť súčasťou tímu? Staň sa dobrovoľníkom MCS a pomôž rozvíjať budúcich lídrov Slovenska.</p>
+        </div>
+        <ButtonPrim
+            class="inline-flex w-60 items-center justify-center"
+            text="Stať sa partnerom"
+            :onClick="() => $router.push('/partners')"
+        />
+      </div>
+      <NuxtImg class="relative xl:-right-[730px] lg:-right-[550px] md:-right-[450px] md:-top-[200px] sm:-right-[400px] sm:-top-[200px] -right-[200px] -top-[40px] img" src="/components/join-us.png" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ButtonPrim from "~/components/buttonPrim.vue";
+</script>
+
+<style>
+
+@media screen and (max-width: 480px) {
+  .img {
+    display: none;
+  }
+
+  .cont {
+    height: auto;
+  }
+}
+
+</style>
