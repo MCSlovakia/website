0f7f6eb6d3f02e76299d28ebb4b7d6efd3a787a9
Vivien Márton <vivien.marton@spsehalova.sk>
2025-09-05 10:03:24 +0200

    Fix image position

--- app/pages/support-us.vue
+++ app/pages/support-us.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class=" bg-mcs-blue min-h-screen lg:px-auto px-5 flex justify-center lg:items-end items-end overflow-visible sm:overflow-x-hidden">
-    <div class="xl:w-[1180px] lg:w-[940px] w-full flex lg:pt-0 pt-14 lg:flex-row md:fle flex-col justify-between lg:items-center items-center">
+  <div class=" bg-mcs-blue min-h-screen lg:px-auto px-5 flex justify-center items-end overflow-y-visible overflow-x-hidden">
+    <div class="xl:w-[1180px] lg:w-[940px] w-full flex lg:pt-0 pt-14 flex-row items-center">
       <div class="w-[580px] flex flex-col gap-6">
     <h1 class="text-txt-white-prim md:text-5xl sm:text-3xl text-2xl">{{ t('support.hero.title') }}</h1>
     <p class="text-txt-white-sec md:text-[18px] text-base">{{ t('support.hero.desc') }}</p>
         <div class="flex gap-4 sm:flex-row flex-col">
           <ButtonPrim
       :text="t('support.hero.ctaWays')"
-              :onClick="() => $router.push('/support-us#ways-to-support')"
+              :onClick="() => $router.push(localePath('/support-us') + '#ways-to-support')"
           />
           <ButtonSec
               class="text-txt-white-prim"
       :text="t('support.hero.ctaPartner')"
-              :onClick="() => $router.push('/partners')"
+              :onClick="() => $router.push(localePath('/partners'))"
           />
         </div>
       </div>
-     <div class="lg:flex-shrink-0 lg:w-auto w-full lg:mt-0 mt-8 lg:relative lg:-left-10 sm:flex sm:justify-end flex justify-center">
-        <NuxtImg
+      <div>
+      <NuxtImg
             src="/components/support/hero.png"
-            class="md:h-[800px] md:w-auto h-full object-contain lg:object-right md:object-right-bottom xs:object-bottom object-center"
+            class="block relative right-0 bottom-0 h-[650px] self-end"
             alt="Hero Image"
         />
-      </div>
     </div>
-  </div>
+    </div>
 
+  </div>
   <div class="flex lg:mx-auto mx-5 justify-center">
     <div class="flex flex-col md:gap-6 gap-5 md:mt-[90px] mt-[50px] xl:w-[1180px] lg:w-[940px] w-full">
   <h2 class="md:text-[40px] text-3xl">{{ t('support.enables.title') }}</h2>
