ad93cef22f18d4b3c91d04a4b677c4a8e55259d3
mrtineu <senkyr.martin@gmail.com>
2026-03-12 21:16:08 +0100

    Add Google Analytics integration with runtime configuration

--- app/app.vue
+++ app/app.vue
@@ -7,3 +7,25 @@
   </div>
 </template>
 
+<script setup lang="ts">
+const { public: publicConfig } = useRuntimeConfig();
+const gaId = publicConfig.gaId as string | undefined;
+
+if (gaId) {
+  useHead({
+    script: [
+      {
+        async: true,
+        src: `https://www.googletagmanager.com/gtag/js?id=${gaId}`
+      },
+      {
+        children: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+gtag('config', '${gaId}');`
+      }
+    ]
+  });
+}
+</script>
+
